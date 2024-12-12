d9f0f270593444358c683cab3369d65c64f4737c
gurgenufuk12 <ufuk.gurgen@outlook.com>
2024-12-12 17:32:04 +0300

    feat: HomePage implemented

---
+++ movie-app/src/pages/HomePage.tsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchMovies } from "../store/moviesSlice";
+import { RootState, AppDispatch } from "../store";
+import SearchBar from "../components/SearchBar";
+import MovieList from "../components/MovieList";
+import { CircularProgress, Alert } from "@mui/material";
+
+const HomePage: React.FC = () => {
+  const dispatch = useDispatch<AppDispatch>();
+  const { movies, totalResults, loading, error } = useSelector(
+    (state: RootState) => state.movies
+  );
+
+  const [searchTerm, setSearchTerm] = useState("Pokemon");
+  const [filters, setFilters] = useState<{ year?: string; type?: string }>({});
+  const [currentPage, setCurrentPage] = useState(1);
+
+  useEffect(() => {
+    dispatch(
+      fetchMovies({ search: searchTerm, page: currentPage, ...filters })
+    );
+  }, [dispatch, searchTerm, currentPage, filters]);
+
+  const handleSearch = (term: string) => {
+    setSearchTerm(term);
+    setCurrentPage(1);
+  };
+
+  const handleFilter = (newFilters: { year?: string; type?: string }) => {
+    setFilters(newFilters);
+    setCurrentPage(1);
+  };
+
+  const handlePageChange = (page: number) => {
+    setCurrentPage(page);
+  };
+
+  return (
+    <div>
+      <SearchBar onSearch={handleSearch} />
+      {loading && <CircularProgress />}
+      {error && <Alert severity="error">{error}</Alert>}
+      {!loading && !error && <MovieList movies={movies} />}
+    </div>
+  );
+};
+
+export default HomePage;
